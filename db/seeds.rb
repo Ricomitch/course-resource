504df8cebc4627cb93817c79781b3d360ae8e370
Rico <ricomitch@icloud.com>
2020-09-16 22:27:06 -0400

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,53 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-courses = Course.create 
+courses = Course.create ([
+
+{ 
+  name: "General Assembly",
+  image_url: "image1"
+}, 
+{ 
+  name: "Devmountain",
+  image_url: "image2"
+},
+{ 
+  name: "Flatiron School",
+  image_url: "image3" 
+}, 
+{ 
+  name: "App Academy",
+  image_url: "image4" 
+}, 
+{ 
+  name: "Coding Dojo",
+  image_url: "image5" 
+}, 
+{ 
+  name: "Hack Reactor",
+  image_url: "image6" 
+},
+{ 
+  name: "Coding Temple",
+  image_url: "image7" 
+}, 
+{ 
+  name: "Codesmith",
+  image_url: "image8" 
+}
+])
+
+reviews = Review.create([
+  {
+    title: 'The best bootcamp ever',
+    description: 'We learned soo much and i got a job',
+    score: 5,
+    course: courses.first
+  },
+  {
+    title: 'Worst bootcamp ever',
+    description: 'We learned nothing and i didnt get a job',
+    score: 1,
+    course: courses.first
+  }
+])
