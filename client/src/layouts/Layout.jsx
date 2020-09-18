0d1f3f8852e515139cc904a01873d38b7e8b0f9f
Rico <ricomitch@icloud.com>
2020-09-18 14:38:08 -0400

    fixed server issue

--- client/src/layouts/Layout.jsx
+++ client/src/layouts/Layout.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from '../components/Header'
 
 const Layout = (props) => {
   return (
     <>
-      <Header/>
+      <Header />
       <main>
         {props.children}
       </main>
