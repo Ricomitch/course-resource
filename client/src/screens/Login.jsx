ee46e841f8af74c3a58d268d4ae7c37ec63e0f20
Rico <ricomitch@icloud.com>
2020-09-19 02:09:41 -0400

    set up login

--- client/src/screens/Login.jsx
+++ client/src/screens/Login.jsx
@@ -1,6 +1,43 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
+export const Login = () => {
+  const [formData, setFormData]= useState({
+    email: '',
+    password: ''
+   })
+  const { email, password } = formData;
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }))
+  }
+
+  return (
+    <form>
+      <h3>Login</h3>
+      <label>Email:
+        <input
+          type='text'
+          name='email'
+          value={email}
+          onChange={handleChange} />
+      </label>
+      <label>Password:
+        <input
+          type='password'
+          name='password'
+          value={password}
+          onChange={handleChange} />
+      </label>
+      <Link to='/register'>Register</Link>
+      <button>Submit</button>
+
+  </form>
+  )
+}
 
 
