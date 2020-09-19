87e028bf0df0af28570b9aa36bd8bc18d56df220
Rico <ricomitch@icloud.com>
2020-09-19 02:08:53 -0400

    set up header blue print

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 import Layout from './layouts/Layout';
+import { Login } from './screens/Login';
 
 function App() {
   return (
     <Layout>
-      
+      <Switch>
+        <Route path='/login'>
+        <Login />
+        </Route>
+        {/* <Route path='/register'>
+          <component />
+        </Route> */}
+
+        {/* <Route exact path="/" component={Courses} />
+        <Route exact path="/course/:slug" component={Course}/> */}
+
+      </Switch>
     </Layout>
   );
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <header>
       <h1>Course Resource</h1>
+      <Link to='/login'>Login/Register</Link>
     </header>
   )
 }
