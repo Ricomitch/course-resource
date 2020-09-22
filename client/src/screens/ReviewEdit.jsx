99f3cccf6b6db2e72c0603c2b07865e4f07caf7d
Rico <ricomitch@icloud.com>
2020-09-22 10:24:05 -0400

    styling

--- client/src/screens/ReviewEdit.jsx
+++ client/src/screens/ReviewEdit.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { useParams, useHistory } from 'react-router-dom'
 import { getOneReview } from '../services/reviews';
-import './Review.css'
+import './ReviewEdit.css'
 
 
 
